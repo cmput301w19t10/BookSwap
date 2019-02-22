b7dd06386400fa22b198425889fdccbefa7cc533
BowenH98 <42704487+BowenH98@users.noreply.github.com>
2019-02-22 16:24:31 -0700

    update User class test

--- app/src/test/java/com/example/bookswap/ExampleUnitTest.java
+++ app/src/test/java/com/example/bookswap/ExampleUnitTest.java
@@ -18,62 +18,55 @@ import static org.junit.Assert.assertEquals;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
-    }
 
+    //test adding book and getting book
     @Test
     public void BookAddAndGet_isCorrect(){
         DataBaseUtil u = new DataBaseUtil();
-        Book newBook = new Book("1","1","1","1","1");
-        u.AddNewBook(newBook);
-        ArrayList testArray;
-        testArray = u.getBooks(1);
-        assertEquals("1",testArray.get(0));
+        Book newBook1 = new Book("1","1","1","1","1");
+        Book newBook2 = new Book("2","1","2","1","1");
+        Book newBook3 = new Book("3","1","3","1","1");
+        Book newBook4 = new Book("4","1","4","1","1");
+        u.AddNewBook(newBook1);
+        u.AddNewBook(newBook2);
+        u.AddNewBook(newBook3);
+        u.AddNewBook(newBook4);
+        ArrayList testArray1 = u.getBooks(1);
+        ArrayList testArray2 = u.getBooks(2);
+        ArrayList testArray3 = u.getBooks(3);
+        ArrayList testArray4 = u.getBooks(4);
+        Book testBook1 = (Book) testArray1.get(0);
+        Book testBook2 = (Book) testArray1.get(1);
+        Book testBook3 = (Book) testArray1.get(2);
+        Book testBook4 = (Book) testArray1.get(3);
+        assertEquals("1",testBook1.getStatus());
+        assertEquals("2",testBook2.getStatus());
+        assertEquals("3",testBook3.getStatus());
+        assertEquals("4",testBook4.getStatus());
     }
 
+    //test adding User and getting User
     @Test
-    public void AddandGetUserPassword_isCorrect(){
+    public void AddAndGetUser_isCorrect(){
         DataBaseUtil u = new DataBaseUtil();
-        u.addPassword("1","1");
-        String testPassword = u.getPassword("1");
-        assertEquals("1",testPassword);
+        User user = new User("1","1","1","1");
+        u.addNewUser(user);
+        User testUser = u.getOneUser("Bowen");
+        assertEquals("1",testUser.getEmail());
     }
 
-    @Test
-    public void AddandGetUserEmail_isCorrect(){
-        DataBaseUtil u = new DataBaseUtil();
-        u.addEmail("1","1");
-        String testEmail = u.getEmail("1");
-        assertEquals("1",testEmail);
-    }
-
-    @Test
-    public void AddandGetUserAddress_isCorrect(){
-        DataBaseUtil u = new DataBaseUtil();
-        u.addAddress("1","1");
-        String testAddress = u.getAddress("1");
-        assertEquals("1",testAddress);
-    }
-
-    @Test
-    public void AddandGetUserPhone_isCorrect(){
-        DataBaseUtil u = new DataBaseUtil();
-        u.addPhone("1","1");
-        String testPhone = u.getPhone("1");
-        assertEquals("1",testPhone);
-    }
-
+    //test changing status
     @Test
     public void chageStatus_isCorrect(){
         DataBaseUtil u = new DataBaseUtil();
-        u.changeStatus(2);
+        u.changeStatus(1);
         ArrayList testArray;
         testArray = u.getBooks(1);
-        assertEquals(2,testArray.get(2));
+        Book testbook = (Book) testArray.get(0);
+        assertEquals("1",testbook.getStatus());
     }
-    
+
+    //test request adding
     @Test
     public void Request_isCorrect(){
         DataBaseUtil u = new DataBaseUtil();
@@ -81,4 +74,6 @@ public class ExampleUnitTest {
         boolean testRequest = u.checkNewRequest("Bowen");
         assertEquals(true,testRequest);
     }
+
+
 }
