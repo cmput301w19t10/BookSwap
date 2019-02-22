6ae7dad0f6178a9c1aad0fb012ed0827c1531452
BowenH98 <42704487+BowenH98@users.noreply.github.com>
2019-02-22 16:25:58 -0700

    update User Class

---
+++ app/src/main/java/com/example/bookswap/User.java
@@ -0,0 +1,31 @@
+package com.example.bowenhu.bookswap;
+
+public class User {
+    private String name;
+    private String phone_number;
+    private String address;
+    private String email;
+
+    public User(String name, String phone_number, String email, String address){
+        this.name = name;
+        this.phone_number = phone_number;
+        this.address = address;
+        this.email = email;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getPhone_number(){
+        return this.phone_number;
+    }
+
+    public String getAddress(){
+        return this.address;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+}
