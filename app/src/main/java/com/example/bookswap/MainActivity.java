1b40fa2d9d9b238e7437fdefb3bae0488a155e9e
Yifu <yifu3@ualberta.ca>
2019-02-24 13:33:51 -0700

    try to fix conflict

---
+++ app/src/main/java/com/example/bookswap/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.bookswap;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * called when the user tap the BOOK button
+     *
+     * @param view
+     */
+    public void Dummy(View view){
+        //Do something in response to button
+        Intent intent = new Intent(this, BookMainActivity.class);
+        startActivity(intent);
+    }
+}
