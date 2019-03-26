efe80078921bd3414dccacf39d799e159ca0c34c
Yifu <yifu3@ualberta.ca>
2019-03-26 10:46:07 -0600

    Swap class lack map funtion

--- app/src/main/java/com/example/bookswap/Swap.java
+++ app/src/main/java/com/example/bookswap/Swap.java
@@ -3,32 +3,78 @@ package com.example.bookswap;
 import java.util.Date;
 //Unimplemented class
 public class Swap {
+    private String location;
+    private String comment;
+    private String borrowname;
+    private String ownername;
+    private Date date;
+    private boolean borrowerPermit;
+    private boolean ownerPermit;
+
 
     public Swap(){}
 
     //TODO find how google maps stores GPS location
-    public void setLocation(String Location){}
+    public void setLocation(String Location){
+        this.location = Location;
+    }
 
-    public void setComment(String Comment){}
+    public void setComment(String Comment){
+        this.comment = Comment;
+    }
 
-    public void setBorrower(String BorrowerName){}
+    public void setBorrower(String BorrowerName){
+        this.borrowname = BorrowerName;
+    }
 
-    public void setOwner(String OwnerName){}
+    public void setOwner(String OwnerName){
+        this.ownername = OwnerName;
+    }
 
-    public void setDate(Date Date){}
+    public void setDate(Date Date){
+        this.date = Date;
+    }
 
-    public String getLocation(){return "1";}
+    public void setBorrowerPermit(boolean borrowerPermit){
+        this.borrowerPermit = borrowerPermit;
+    }
 
-    public String getComment(){return "1";}
+    public void setOwnerPermit(boolean ownerPermit){
+        this.ownerPermit = ownerPermit;
+    }
+
+    public String getLocation(){
+        return location;
+        }
+
+    public String getComment(){
+        return comment;
+    }
 
     //Should reuturn date type
     public Date getDate(){
-        Date date = new Date("2019-01-01");
         return date;
     }
 
-    public String getOwner(){return "1";}
+    public String getOwner(){
+        return ownername;
+    }
 
-    public String getBorrower(){return  "1";}
+    public String getBorrower(){
+        return borrowname;
+    }
+
+    public boolean isBorrowerPermit() {
+        return borrowerPermit;
+    }
+
+    public boolean isOwnerPermit() {
+        return ownerPermit;
+    }
+
+    public boolean checkBorrowerOwnerPermit(){
+        return borrowerPermit && ownerPermit;
+    }
+
 }
 
