0e1356bbf00fc52ae627218c844ee2b5c312a8f6
uKira <darkra98@gmail.com>
2019-03-29 20:43:19 -0600

    finished writing view map location

---
+++ app/src/main/java/com/example/bookswap/MapViewActivity.java
@@ -0,0 +1,48 @@
+package com.example.bookswap;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapViewActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private LatLng point = getIntent().getParcelableExtra("point");
+    private Button setLocation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_select);
+
+        setLocation.findViewById(R.id.setMeeting);
+        setLocation.setEnabled(false);
+
+        SupportMapFragment myMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.selectMap);
+        myMapFragment.getMapAsync(this);
+        ;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map){
+        final Marker meetup = map.addMarker(new MarkerOptions()
+                .position(point)
+                .title("Meetup here!")
+                .draggable(false));
+        map.moveCamera(CameraUpdateFactory.zoomTo(15));
+        map.moveCamera(CameraUpdateFactory.newLatLng(point));
+    }
+}
