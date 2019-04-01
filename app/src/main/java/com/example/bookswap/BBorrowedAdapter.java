a14bc7e865a508674953e74271e8e8901e37e0b6
cmeng2 <cmeng2@ualberta.ca>
2019-04-01 06:15:47 -0600

    img

--- app/src/main/java/com/example/bookswap/BBorrowedAdapter.java
+++ app/src/main/java/com/example/bookswap/BBorrowedAdapter.java
@@ -11,6 +11,8 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
 import java.util.ArrayList;
 
 /**
@@ -106,8 +108,11 @@ public class BBorrowedAdapter extends ArrayAdapter<Book> {
                 getContext().startActivity(returnBook);
             }
         });
-        if (element.getImage() != null) {
-            holder.bookcover.setImageBitmap(element.getImage());
+
+        if (element.getImageUrl()!= null){
+            Picasso.get()
+                    .load(element.getImageUrl())
+                    .into(holder.bookcover);
         }
 
 
